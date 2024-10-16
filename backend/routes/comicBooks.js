cbf4f2330927017d7b200c6216a60bb0983a2f7d
JanaviK <kharatjanavi16@gmail.com>
2024-10-17 00:33:37 +0530

    Add route for getting comic book by id

--- backend/routes/comicBooks.js
+++ backend/routes/comicBooks.js
@@ -6,5 +6,8 @@ const router = express.Router();
 //ROUTE - get all comic book list
 router.get('/',comicBooksController.getAllComicBooks);
 
+//ROUTE - get a single comic book details
+router.get('/:id',comicBooksController.getComicBookById);
+
 
 module.exports = router;
