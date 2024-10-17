a4dec7b7a2c336164b10151df00a68e9ae3d49c5
JanaviK <kharatjanavi16@gmail.com>
2024-10-17 21:45:14 +0530

    Add sequelize input validations to fields

--- backend/models/comicbook.js
+++ backend/models/comicbook.js
@@ -17,31 +17,93 @@ module.exports = (sequelize, DataTypes) => {
     book_name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Book name is required,',
+        },
+        len: {
+          args: [3],
+          msg: 'Book name must be at least 3 characters',
+        }
+      },
     },
     author_name:  {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Author name is required',
+        },
+        len: {
+          args: [3],
+          msg: 'Author name must be at least 3 characters',
+        }
+      },
     },
     year_of_publication: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate: {
+        isInt: {
+          msg: 'Year of publication must be an integer.',
+        },
+        max: new Date().getFullYear(),
+        min: 1900,
+      },
     },
     price: {
       type: DataTypes.FLOAT,
       allowNull: false,
+      validate: {
+        isFloat: {
+          msg: 'Price must be a number',
+        },
+        min: {
+          args: [0],
+          msg: 'Price must be greater than zero',
+        },
+      }, 
     },
     discount: {
       type: DataTypes.INTEGER,
       allowNull: true,
+      validate: {
+        isInt: {
+          msg: 'Discount mus be a number',
+        },
+        min: {
+          args: [0],
+          msg: 'Discount cannot be negative',
+        },
+        max: {
+          args: [100],
+          msg: 'Discount cannot exceed 100%',
+        },
+      },
     },
     number_of_pages: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate:{
+        isInt: {
+          msg: 'Number of pages must be a positive integer',
+        },
+        min: {
+          args: [1],
+          msg: 'Number of pages must be at least 1',
+        },
+      },
     },
     book_condition:  {
       type: DataTypes.ENUM,
       values: ['NEW','USED'],
-      defaultValue: 'NEW'
+      defaultValue: 'NEW',
+      validate:{
+        isIn:{
+          args: [['NEW','USED']],
+          msg: 'Book condition must be either "NEW" or "USED',
+        },
+      },
     },
     description:  {
       type: DataTypes.STRING,
@@ -50,6 +112,15 @@ module.exports = (sequelize, DataTypes) => {
     category:  {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: {
+            msg: 'Category is required',
+        },
+        len: {
+          args: [3],
+          msg: 'Category name must be at least 3 characters',
+        }
+      },
     },
     user_id: {
       type: DataTypes.INTEGER,
