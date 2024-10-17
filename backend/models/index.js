0a7ef981774a9089906de6218fddd247b10295f4
JanaviK <kharatjanavi16@gmail.com>
2024-10-17 17:48:13 +0530

    Add host and dialect in index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -13,8 +13,8 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config, {
-      host: config.host,
+  sequelize = new Sequelize(config.database, config.username, config.password, config,{
+    host: config.host,
       dialect: config.dialect,
       logging: console.log,
   });
@@ -42,5 +42,6 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
